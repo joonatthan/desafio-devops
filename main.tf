899c703469c5acade7190518f4f5c2e86c36dc3f
Jonatha Oliveira <o.jonathan.alves@gmail.com>
2023-12-12 12:58:04 -0300

    adicionando security group personalizado

--- main.tf
+++ main.tf
@@ -2,6 +2,40 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_security_group" "security_group_desafio" {
+  name        = "security-group-desafio"
+  description = "Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_instance" "desafio" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -11,4 +45,7 @@ resource "aws_instance" "desafio" {
   tags = {
     Name = "desafio-devops"
   }
+
+  vpc_security_group_ids = [ aws_security_group.security_group_desafio.id ]
+
 }
